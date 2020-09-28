9f02020e57251ca331ddd510590fb35da68bbb48
Lahiru Udawaththa <45584797+lahiruroot@users.noreply.github.com>
2020-09-28 14:03:44 +0530

    Add files via upload

---
+++ audiobook.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import pyPDF2
+book = open('machine.pdf','rb')
+pdfReader = pyPDF2.pdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+speaker = pyttsx3.int()
+for num in pages:
+    page = pdfReader.getPage(7, pages)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndwait()
