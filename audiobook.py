d9531859055305547fc64898c8d23a9e1eb903d4
Amilashan98 <72974018+Amilashan98@users.noreply.github.com>
2020-10-16 16:01:34 +0530

    Update audiobook.py

--- audiobook.py
+++ audiobook.py
@@ -1,12 +1,12 @@
 import pyttsx3
 import pyPDF1
 book = open('ttsl.pdf','rb')//get value
-pdfReader = pyPDF1.pdfFileReader(book)
+pdfReader = pyPDF1.pdfFileReader(booktext)
 pages = pdfReader.numPages
 print(pages)
 speaker = pyttsx3.int()
 for num in pages:
     page = pdfReader.getPage(8, pages)
     text = page.extractText()
-    speaker.speek(text)
+    speaker.speek(mytext)
     speaker.runAndwait()
